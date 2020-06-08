0f1b3589ca109bf1083a75a0ba33d0fc125eb71e
Arkadiusz.Ptak <arkadiusz.ptak@cybercom.com>
2020-06-08 20:02:42 +0200

    Authentication + Authorization

--- src/main/java/com/ptak/OrganizerEureka/API/AuthenticationController.java
+++ src/main/java/com/ptak/OrganizerEureka/API/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.ptak.OrganizerEureka.model.AuthenticationRequest;
 import com.ptak.OrganizerEureka.model.AuthenticatonResponse;
 import com.ptak.OrganizerEureka.services.MyUserDetailsService;
 import com.ptak.OrganizerEureka.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("security")
+@Slf4j
 public class AuthenticationController {
 
     @Autowired
---
+++ src/main/java/com/ptak/OrganizerEureka/services/PersistanceService.java
@@ -0,0 +1,24 @@
+package com.ptak.OrganizerEureka.services;
+
+import com.ptak.OrganizerEureka.User.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class PersistanceService {
+
+    @Autowired
+    RestTemplate restTemplate;
+    @Value("${persistance.application.name}")
+    private String persistanceApplicationName;
+
+    protected User getUserByUsernameFromDatabase(String username) {
+
+        UserDTO userDTO = restTemplate.getForObject("http://localhost:8083/user/"+username, UserDTO.class);
+        return userDTO.toUser();
+    }
+}
